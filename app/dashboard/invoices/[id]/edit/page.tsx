953b011e5759d6fe6b1bc42cb849b1889df26ef3
Valeriy Danets <dantesvv@gmail.com>
2023-11-28 11:55:57 +0200

    Were added metadata into Pages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/dashboard/invoices/edit-form';
 import Breadcrumbs from '@/app/dashboard/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,11 @@
 import Form from '@/app/dashboard/invoices/create-form';
 import Breadcrumbs from '@/app/dashboard/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create',
+};
 
 export default async function Page() {
   const customers = await fetchCustomers();
