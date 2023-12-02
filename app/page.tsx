d5a17facb871252bc5af038561941842bbbb97ad
Valeriy Danets <dantesvv@gmail.com>
2023-12-02 12:08:15 +0200

    Was implemented the feature of Dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import NavLinks from './dashboard/nav-links';
+import DarkModeButton from './ui/darkmode-button';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 items-center justify-evenly rounded-lg bg-blue-500 p-4">
         <AcmeLogo />
         <NavLinks />
+        <DarkModeButton />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
